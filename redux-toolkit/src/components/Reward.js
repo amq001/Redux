e7170a568303588a4251aaf6292acae300ffb837
amq001 <aqibmehtab001@gmail.com>
2025-08-17 12:40:05 +0500

    Changes

---
+++ redux-toolkit/src/components/Reward.js
@@ -0,0 +1,21 @@
+import { useDispatch, useSelector } from "react-redux";
+import { increment, incrementByAmount } from "../reducers/reward";
+
+function Reward() {
+  const points = useSelector((state) => state.reward.points);
+  const dispatch = useDispatch();
+  return (
+    <div className="card">
+      <div className="container">
+        <h4>
+          <b>Reward Component</b>
+        </h4>
+        <h3>Total Point : {points}</h3>
+        <button onClick={() => dispatch(increment())}>Increment +</button>
+        <button onClick={() => dispatch(incrementByAmount(7))}>Increment By Seven +</button>
+      </div>
+    </div>
+  );
+}
+
+export default Reward;
